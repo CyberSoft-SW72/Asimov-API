5a95a0bf564ad325725dac13a3fd165375735670
adrianVelasco <64821523+Luadveand@users.noreply.github.com>
2022-10-25 17:44:59 -0500

    First commit
    This commit contains the work made in the last few weeks

---
+++ README.md
@@ -0,0 +1,15 @@
+# Backend Asimov 🎓
+
+## Description
+This repository contains the updated backend for the asimov project.
+
+## Team CyberSoft 👨‍💻
+* Alonso Olbarte Chirinos Paredes - U201924282
+* Luis Adrian Velasco Andaluz - u201414604
+* Antony Sthif Carrasco Cunya - U20181B702
+* Patrick Viery Zurita Tenorio - U201621873
+* Oscar Enrique Palomino Medina - U201822717
+
+## Documentation
+
+* version 2.0.0 25/10/2022
---
+++ src/main/java/com/cybersoft/asimovapi/AsimovApiApplication.java
@@ -0,0 +1,34 @@
+package com.cybersoft.asimovapi;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@SecurityScheme(name = "acme", scheme = "bearer", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
+@OpenAPIDefinition
+@SpringBootApplication
+@EnableJpaAuditing
+public class AsimovApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsimovApiApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+            }
+        };
+    }
+
+}
